df9b08f8e29dd3b245d6780071570cb03122135d
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-03-25 23:27:33 -0500

    Autoload de Archivos
    Muchas veces cuando estamos empezando a programar, al crear una
    aplicación y al tener varios archivos, necesitamos funcionalidades de
    otros, y para ello debemos de cargar los archivos que necesitamos en el
    que estamos trabajando actualmente. Y en PHP la solución es cargarlo con
    require o include.
    
    En este caso como estamos trabajando con Modelo Vista y Controlador,
    además de cargar el archivo, debemos cargar el método que se solicita y
    pasar los parámetros. Para ello creamos una clase que se encargue se
    hacer eso. La clase Autoload.
    
    En la clase Autoload nosotros cargamos el archivo, y los métodos que se
    soliciten. Creamos también un archivo config.php para cargar aquí toda
    la configuración de nuestra aplicación. Y finalmente la llamamos
    llamamos el  archivo config, que es genérico, para cargar la clase
    Autoload y que está se encargue de tener lo necesario para el
    funcionamiento de la aplicación.

---
+++ autoload.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Autoload {
+
+	public function __construct(){
+		#Validar si existe una clase y cargarla en la aplicación
+		spl_autoload_register(function($class){
+			$file = 'Controller/'.$class.'.php';
+			if (file_exists($file)){
+				require_once($file);
+			} else if (file_exists('libs/'.$class.'.php')){
+				require_once('libs/'.$class.'.php');
+			} else if (file_exists('libs/'.$class.'.php')){
+				require_once('libs/'.$class.'.php');
+			} else {
+				echo 'No existe el Controlador '.$class.'<br>';
+			}
+		});
+	}
+
+	public function load($_controller, $_method, $_params){
+		if (class_exists($_controller)){
+			$controller = new $_controller($_controller, $_method);
+			if (method_exists($_controller, $_method)){
+				$controller->method(_params);
+			} else {
+				echo 'No existe el método '.$_method.' de la clase '.$_controller.'<br/>';
+			}
+		} else {
+			echo 'No existe la clase '.$_controller.'<br/>';
+		}
+	}
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once('config.php');
 #Validamos que esté la variable url definida, si no le asignamos un controlador por defecto
 $ruta = isset($_GET['url']) ? $_GET['url'] : 'index/inicio';
 
@@ -10,12 +10,15 @@ if(isset($ruta[0])){
 	$controller = $ruta[0];
 }
 if(isset($ruta[1])){
-	$metodo = $ruta[1];
+	$method = $ruta[1];
 }
 if(isset($ruta[2])){
 	$params = $ruta[2];
 } else {
 	$params = '';
 }
+
+$load = new Autoload();
+$load->load($controller, $method, $params);
 
 ?>
